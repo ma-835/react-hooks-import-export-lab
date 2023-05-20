8540cec8f56cfb60db9bb3bc41186606addec673
Mahmoud Abdelkarim <mabdelkarim835@gmail.com>
2023-05-20 12:28:05 -0400

    complete lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,6 @@ import React from "react";
 import {username, city} from "../data/user";
 
 function Home() {
-
-const username = "Your Name"
-const city = "Your City"
-
   return (
     <div id="home">
       <h1 style = {{ color: 'firebrick' }} >
